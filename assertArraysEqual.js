589ddc11e927dae3f17bdbe00373d07c9a9d8a56
Rita Tang <doggymint@gmail.com>
2023-08-22 16:14:41 -0400

    revise assetArraysEqual function logic

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,22 +1,32 @@
-const assertArraysEqual = function (array1, array2) {
-  // check if two arrays of the same length
-  if (array1.length !== array2.length) {
-    console.log("❌❌Assertion Failed: the arrays are of different length");
-    return;
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(
+      `✅✅Assertion Passed: ${JSON.stringify(array1)} === ${JSON.stringify(
+        array2
+      )}`
+    );
+  } else {
+    console.log(
+      `❌❌Assertion Failed: ${JSON.stringify(array1)} !== ${JSON.stringify(
+        array2
+      )}`
+    );
   }
-  console.log("array1:", array1, "array2:", array2);
+};
+
+const eqArrays = function(array1, array2) {
+  // check if two arrays are of the same length
+  if (array1.length !== array2.length) return false;
   // iterate through each array
   for (let i = 0; i < array1.length; i++) {
     // compare the values from both array
-    // log 'assertion failed' the values being compared when a mismtach is found
+    // return result as false whenever a mismatch is found
     if (array1[i] !== array2[i]) {
-      console.log(`❌❌Assertion Failed: ${array1[i]} !== ${array2[i]}`);
-      return;
+      return false;
     }
   }
-  // log 'assertion passed' as two arrays are the same when it gets to this point
-  console.log("✅✅Assertion Passed");
-  return;
+  // return the result as true when every element matches
+  return true;
 };
 
 // Test code
