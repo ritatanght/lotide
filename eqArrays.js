d39c26d1b05043c488f2e286b19e4d7ea27aa887
Rita Tang <doggymint@gmail.com>
2023-08-22 14:53:11 -0400

    add eqArrays

---
+++ eqArrays.js
@@ -0,0 +1,28 @@
+const eqArrays = function(array1, array2) {
+  // check if two arrays of the same length
+  if (array1.length !== array2.length) return false;
+  // iterate through each array
+  for (let i = 0; i < array1.length; i++) {
+    // compare the values from both array
+    // return result as false whenever a mismatch is found
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  // return the result as true when every element matches
+  return true;
+};
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
