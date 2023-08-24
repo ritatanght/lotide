cbde5107c7d5708764c5775c3e083757c325bbd5
Rita Tang <doggymint@gmail.com>
2023-08-24 12:15:11 -0400

    add test case for undefined

--- findKey.js
+++ findKey.js
@@ -1,4 +1,4 @@
-const assertEqual = function(actual, expected) {
+const assertEqual = function (actual, expected) {
   if (actual === expected) {
     console.log(`✅✅Assertion Passed: ${actual} === ${expected}`);
   } else {
@@ -43,4 +43,19 @@ assertEqual(
     (x) => x.stars === 3
   ),
   "Akaleri"
+);
+
+assertEqual(
+  findKey(
+    {
+      "Blue Hill": { stars: 1 },
+      Akaleri: { stars: 3 },
+      noma: { stars: 2 },
+      elBulli: { stars: 3 },
+      Ora: { stars: 2 },
+      Akelarre: { stars: 3 },
+    },
+    (x) => x.stars === 4
+  ),
+  undefined
 );
