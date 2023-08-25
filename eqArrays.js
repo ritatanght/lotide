5b7b28c578e029062188201d1534ef19f5586dfc
Rita Tang <doggymint@gmail.com>
2023-08-25 17:07:05 -0400

    implement recursion for nested arrays

--- eqArrays.js
+++ eqArrays.js
@@ -3,10 +3,15 @@ const eqArrays = function(array1, array2) {
   if (array1.length !== array2.length) return false;
   // iterate through each array
   for (let i = 0; i < array1.length; i++) {
-    // compare the values from both array
-    // return result as false whenever a mismatch is found
-    if (array1[i] !== array2[i]) {
-      return false;
+    // if both values are array, call the function itself
+    if (Array.isArray(array1[i]) && Array.isArray(array2[i])) {
+      if (!eqArrays(array1[i], array2[i])) {
+        return false;
+      }
+    } else {
+      // compare the values from both array
+      // return result as false whenever a mismatch is found
+      if (array1[i] !== array2[i]) return false;
     }
   }
   // return the result as true when every element matches
@@ -26,3 +31,7 @@ assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
 
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true);
+assertEqual(eqArrays([[2, 3], [4]],[[2, 3],[4, 5]]), false);
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false);
