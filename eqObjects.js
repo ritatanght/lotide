b4689e4697bab83688a92623f98dc38159969fdf
Rita Tang <doggymint@gmail.com>
2023-08-25 16:47:17 -0400

    add recursion to compare nested objects

--- eqObjects.js
+++ eqObjects.js
@@ -27,11 +27,16 @@ const eqObjects = function(object1, object2) {
   // return false when both objects do not have the same number of keys
   if (Object.keys(object1).length !== Object.keys(object2).length) return false;
   for (let key in object1) {
-    // when both values are array, compare with eqArrays()
-    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
-      if (!eqArrays(object1[key], object2[key])) {
-        return false;
+    //check if both values are object
+    if (typeof object1[key] === "object" && typeof object2[key] === "object") {
+      // when both values are array, compare with eqArrays()
+      if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+        if (!eqArrays(object1[key], object2[key])) {
+          return false;
+        }
       }
+      // both values are objects but not arrays, call itself
+      return eqObjects(object1[key], object2[key]);
     } else {
       // otherwise compare the primitive values and return false in case of mismatch
       if (object1[key] !== object2[key]) {
@@ -60,5 +65,27 @@ const longSleeveMultiColorShirtObject = {
 };
 assertEqual(
   eqObjects(multiColorShirtObject, longSleeveMultiColorShirtObject),
+  false
+);
+
+assertEqual(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), true);
+
+assertEqual(
+  eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }),
+  false
+); // => false
+assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 }), false);
+assertEqual(
+  eqObjects(
+    { a: { x: { y: 0, z: 1 } }, b: 2 },
+    { a: { x: { y: 0, z: 1 } }, b: 2 }
+  ),
+  true
+);
+assertEqual(
+  eqObjects(
+    { a: { x: { y: 2, z: 1 } }, b: 2 },
+    { a: { x: { y: 0, z: 1 } }, b: 2 }
+  ),
   false
 );
